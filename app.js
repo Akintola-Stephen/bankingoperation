31e261d68a56129e635085a6104adf5b516d53f3
Akintola-Stephen <sakintola351@gmail.com>
2023-09-15 00:30:02 +0100

    Fix: Cannot access 'app' before initialization

--- app.js
+++ app.js
@@ -4,14 +4,23 @@ const bankRoute = require("./router/bank.router")
 const cors = require('cors')
 require("dotenv").config();
 
-// Enabled CORS support
-app.use(cors())
 
 const app = express()
+
+
+// Enabled CORS support
+app.use(cors())
 
 const PORT = process.env.PORT || 5000
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.status(404).send({
+        status: 404,
+        error: `Not Found`
+    })
+})
 
 app.use('/api', bankRoute)
 
