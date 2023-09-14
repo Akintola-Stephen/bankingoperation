448c19a92d16db3b0906f94a4017f4a443d50c5e
Akintola-Stephen <sakintola351@gmail.com>
2023-09-14 22:52:52 +0100

    Update: Changed some endpoint names

--- router/bank.router.js
+++ router/bank.router.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const router = express.Router()
+const bankMiddleware = require("../middleware/validation.middleware")
 const bankRoute = require("../controller/bank.controller")
 
 router.post('/user', bankRoute.createBankProfile)
-router.get('/retrieveUserDetails', bankRoute.retrieveUserDetails)
-router.get('/retrieveAllAccountDetails', bankRoute.retrieveAllAccountDetails)
+router.get('/userbankdetails', bankMiddleware.userExist, bankRoute.retrieveUserDetails)
+router.get('/bankdetaiils', bankRoute.retrieveAllAccountDetails)
 
 
 module.exports = router;
