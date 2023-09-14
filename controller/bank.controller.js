03fc41c9653f4fbddf9265309f89a9518dad11bb
Akintola-Stephen <sakintola351@gmail.com>
2023-09-14 15:10:46 +0100

    Added a response and request parameter

--- controller/bank.controller.js
+++ controller/bank.controller.js
@@ -6,13 +6,13 @@ const express = require("express")
 // A function for creating a user, another function to retirve a persons details
 // And a function to retrieve all banks
 const bankObj = {
-    createBankProfile: async() => {
+    createBankProfile: async(req, res) => {
 
     },
-    fetchSingleBank: async() => {
+    fetchSingleBank: async(req, res) => {
 
     },
-    fetchAllBanks: async() => {
+    fetchAllBanks: async(req, res) => {
 
     }
 }
