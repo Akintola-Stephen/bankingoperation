f20939ca787654e090cdb9ec6e8e5494fbb44735
Akintola-Stephen <sakintola351@gmail.com>
2023-09-14 14:42:00 +0100

    Added three functions that performs project topic

---
+++ controller/bank.controller.js
@@ -0,0 +1,21 @@
+const express = require("express")
+
+
+
+// An object that wraps in three functions
+// A function for creating a user, another function to retirve a persons details
+// And a function to retrieve all banks
+const bankObj = {
+    createBankProfile: async() => {
+
+    },
+    fetchSingleBank: async() => {
+
+    },
+    fetchAllBanks: async() => {
+
+    }
+}
+
+
+module.exports = bankObj;
