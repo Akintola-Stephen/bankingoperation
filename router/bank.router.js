09c4e604e80e862448daf4564e049551a08137cd
Akintola-Stephen <sakintola351@gmail.com>
2023-09-14 23:50:17 +0100

    Update: Changed some route names

--- router/bank.router.js
+++ router/bank.router.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const bankMiddleware = require("../middleware/validation.middleware")
 const bankRoute = require("../controller/bank.controller")
 
-router.post('/user', bankRoute.createBankProfile)
+router.post('/user/register', bankRoute.createBankProfile)
 router.get('/userbankdetails', bankMiddleware.userExist, bankRoute.userBankDetails)
 router.get('/bankdetails', bankRoute.accountBankDetails)
 
