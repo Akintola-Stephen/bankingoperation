fe8efbae9f7f9cfcc1540fb7586f5836a457a826
Akintola-Stephen <sakintola351@gmail.com>
2023-09-14 16:44:24 +0100

    Implemented service logics on controller file

--- controller/bank.controller.js
+++ controller/bank.controller.js
@@ -1,18 +1,20 @@
 const express = require("express")
-
-
+const bankService = require("../service/bankService")
 
 // An object that wraps in three functions
 // A function for creating a user, another function to retirve a persons details
 // And a function to retrieve all banks
 const bankObj = {
     createBankProfile: async(req, res) => {
-
+        const { userObj } = req.body;
+        await bankService.createUser(userObj)
     },
-    fetchSingleBank: async(req, res) => {
 
+    retrieveUserDetails: async(req, res) => {
+        const accountNumber = req.accountNumber;
+        await bankService.retrieveDetails(accountNumber)
     },
-    fetchAllBanks: async(req, res) => {
+    retrieveAllAccountDetails: async(req, res) => {
 
     }
 }
