3306ff858c08cf61ef36f0e084b5014a49d4a67e
Akintola-Stephen <sakintola351@gmail.com>
2023-09-15 00:07:25 +0100

    Add; Added cors support

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express")
 const dbConnection = require("./config/db")
+const bankRoute = require("./router/bank.router")
+const cors = require('cors')
 require("dotenv").config();
+
+// Enabled CORS support
+app.use(cors())
 
 const app = express()
 
 const PORT = process.env.PORT || 5000
+
+app.use(express.json());
+
+app.use('/api', bankRoute)
 
 //establish connection to database
 dbConnection()
